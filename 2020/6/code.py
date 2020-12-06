e08a75312d34763ad61c47a462c77a8cf59b3b0a
jakubwujec <jakubwujec98@gmail.com>
2020-12-06 11:15:55 +0100

    d6

--- 2020/6/code.py
+++ 2020/6/code.py
@@ -5,10 +5,21 @@
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
+# z1
+groups = input.split('\n\n')
+counter1 = 0
+for group in groups:
+    group_score = len(set(group.replace('\n','')))
+    counter1 += group_score
+
+print("Part One : "+ str(counter1))
+
+# z2
+counter2 = 0
+groups = list(map(lambda x: x.split('\n'), groups))
+for group in groups:
+    group = list(map(lambda x: set(x), group))
+    counter2 += len(group[0].intersection(*group))
 
 
-print("Part One : "+ str(None))
-
-
-
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(counter2))
