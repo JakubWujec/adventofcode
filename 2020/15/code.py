99ef22bea1e78a3f296628f66712ecd146b3bc1f
jakubwujec <jakubwujec98@gmail.com>
2020-12-15 09:09:28 +0100

    d15

--- 2020/15/code.py
+++ 2020/15/code.py
@@ -2,13 +2,32 @@
 # Author = ?
 # Date = December 2020
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
     input = input_file.read()
 
+input_numbers = list(map(int, input.split(',')))
 
 
-print("Part One : "+ str(None))
+def z1(numbers: [int], _limit):
+    numbers = numbers.copy()
+    last_spoken = {}
+    for index, num in enumerate(numbers):
+        last_spoken[num] = index
+    start = len(numbers)
+
+    for i in range(start, _limit):
+        last_number = numbers[i - 1]
+        if last_number not in last_spoken.keys():
+            # say 0
+            numbers.append(0)
+            last_spoken[last_number] = i - 1
+            if 0 not in last_spoken.keys():
+                last_spoken[0] = i
+        else:
+            numbers.append(i - 1 - last_spoken[last_number])
+            last_spoken[last_number] = i - 1
+    return numbers[-1]
 
 
-
-print("Part Two : "+ str(None))
+print("Part One : " + str(z1(input_numbers, 2020)))
+print("Part Two : " + str(z1(input_numbers, 30000000)))
