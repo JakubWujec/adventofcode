a37164c1341d71a3bd90d12fe243e8dfeef43c77
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-12-04 10:36:30 +0100

    t1

--- aoc2025/day4/code.py
+++ aoc2025/day4/code.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def get_input_data(filename="input.txt"):
     with open((__file__.rstrip("code.py") + filename), "r") as input_file:
         input = input_file.read()
@@ -5,7 +8,35 @@ def get_input_data(filename="input.txt"):
 
 
 def task1(input_data: str):
-    return 0
+    grid = input_data.split("\n")
+    while grid[-1] == "":
+        grid.pop()
+    answer = 0
+
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == "@":
+                adj = get_8_adjacement(grid, r, c)
+                if adj.count("@") < 4:
+                    answer += 1
+    return answer
+
+
+def get_8_adjacement(grid: List[str], row: int, column: int):
+    rows = len(grid)
+    columns = len(grid[0])
+
+    result = []
+
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            n_row = row + dx
+            n_col = column + dy
+
+            if n_row != row or n_col != column:
+                if n_row >= 0 and n_col >= 0 and n_row < rows and n_col < columns:
+                    result.append(grid[n_row][n_col])
+    return result
 
 
 def task2(input_data: str):
