21b80d552dc1bcae4bed5b597184174067044edc
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-12-05 09:47:02 +0100

    part1

--- aoc2025/day5/code.py
+++ aoc2025/day5/code.py
@@ -7,8 +7,60 @@ def get_input_data(filename="input.txt"):
         return input
 
 
+def process_input(input_data: str):
+    rows = input_data.split()
+    ranges = []
+    ids = []
+    for row in rows:
+        if "-" in row:
+            s, e = row.split("-")
+            ranges.append([int(s), int(e)])
+        else:
+            ids.append(int(row))
+
+    return (merge_overlapping_ranges(ranges), ids)
+
+
+def merge_overlapping_ranges(ranges):
+    ranges.sort()
+    rstack = []
+    for r in ranges:
+        rstack.append(r)
+        while len(rstack) >= 2 and rstack[-1][0] <= rstack[-2][1]:
+            pop1 = rstack.pop()
+            pop2 = rstack.pop()
+            rstack.append([min(pop1[0], pop2[0]), max(pop1[1], pop2[1])])
+    return rstack
+
+
+def binary_check(ranges, check):
+    left = 0
+    right = len(ranges)
+
+    while left < right:
+        mid = left + (right - left) // 2
+        mid_range = ranges[mid]
+
+        if check < mid_range[0]:
+            right = mid
+        elif check > mid_range[1]:
+            left = mid + 1
+        else:
+            return True
+
+    return
+
+
 def task1(input_data: str):
-    return 0
+    ranges, ids = process_input(input_data)
+    answer = 0
+    print(ranges)
+    for ingredient_id in ids:
+        if binary_check(ranges, ingredient_id):
+            print(ingredient_id)
+            answer += 1
+
+    return answer
 
 
 def task2(input_data: str):
@@ -17,6 +69,7 @@ def task2(input_data: str):
 
 if __name__ == "__main__":
     input_data = get_input_data()
+
     answer1 = task1(input_data)
     print(f"Answer1: {answer1}")
 
